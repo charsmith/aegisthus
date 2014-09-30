7cbe580b28d4bf2632164f253f51fc4b526b9f53
Daniel Watson <dwatson@netflix.com>
2014-09-30 13:11:16 -0700

    Better handling of deleted columns and ranges.

--- aegisthus-hadoop/src/main/java/com/netflix/aegisthus/mapred/reduce/CassSSTableReducer.java
+++ aegisthus-hadoop/src/main/java/com/netflix/aegisthus/mapred/reduce/CassSSTableReducer.java
@@ -17,10 +17,11 @@ package com.netflix.aegisthus.mapred.reduce;
 
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.cassandra.db.Column;
 import org.apache.cassandra.db.ColumnSerializer;
-import org.apache.cassandra.db.IColumn;
 import org.apache.cassandra.db.OnDiskAtom;
 import org.apache.cassandra.db.RangeTombstone;
 import org.apache.cassandra.db.marshal.BytesType;
@@ -39,20 +40,61 @@ import com.netflix.aegisthus.io.writable.CompositeKey;
 import com.netflix.aegisthus.tools.StorageHelper;
 
 public class CassSSTableReducer extends Reducer<CompositeKey, AtomWritable, Text, Text> {
-    public static class Reduce {
-        private List<OnDiskAtom> columns;
-        private IColumn currentColumn = null;
-        private Long deletedAt = Long.MIN_VALUE;
-        private byte[] key = null;
-        private final OnDiskAtom.Serializer serializer = new OnDiskAtom.Serializer(new ColumnSerializer());
-        private RangeTombstone.Tracker tombstoneTracker;
+    private static final Logger LOG = LoggerFactory.getLogger(CassSSTableReducer.class);
+    FSDataOutputStream dos;
+    Reduce reduce;
 
-        public Reduce() {
-            columns = Lists.newArrayList();
-            // TODO: need to get comparator
-            this.tombstoneTracker = new RangeTombstone.Tracker(BytesType.instance);
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        dos.close();
+        super.cleanup(context);
+    }
+
+    @Override
+    public void reduce(CompositeKey key, Iterable<AtomWritable> values, Context ctx) throws IOException, InterruptedException {
+        reduce = new Reduce();
+
+        for (AtomWritable value : values) {
+            reduce.setKey(value.getKey());
+            if (value.getDeletedAt() > reduce.getDeletedAt()) {
+                reduce.setDeletedAt(value.getDeletedAt());
+            }
+            reduce.addAtom(value.getAtom());
+        }
+        reduce.finalizeReduce();
+        reduce.writeRow(dos);
+        // TODO: batch this
+        ctx.getCounter("aegisthus", "rows_written").increment(1L);
+    }
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        StorageHelper sh = new StorageHelper(context);
+        Path outputDir = new Path(sh.getBaseTaskAttemptTempLocation());
+        FileSystem fs = outputDir.getFileSystem(context.getConfiguration());
+        String filename = String.format("%s-%s-%05d0%04d-Data.db",
+                context.getConfiguration().get("aegisthus.dataset", "keyspace-dataset"), Version.current_version,
+                context.getTaskAttemptID().getTaskID().getId(), context.getTaskAttemptID().getId());
+        Path outputFile = new Path(outputDir, filename);
+        LOG.info("writing to: {}", outputFile.toUri());
+        dos = fs.create(outputFile);
+        super.setup(context);
+    }
+
+    static class Reduce {
+        private final List<OnDiskAtom> columns = Lists.newArrayList();
+        private final OnDiskAtom.Serializer serializer = new OnDiskAtom.Serializer(new ColumnSerializer());
+        // TODO: need to get comparator
+        private final RangeTombstone.Tracker tombstoneTracker = new RangeTombstone.Tracker(BytesType.instance);
+        private OnDiskAtom currentColumn = null;
+        private long deletedAt = Long.MIN_VALUE;
+        private byte[] key;
+
+        public void setKey(byte[] key) {
+            this.key = key;
         }
 
+        @SuppressWarnings("StatementWithEmptyBody")
         public void addAtom(OnDiskAtom atom) {
             if (atom == null) {
                 return;
@@ -62,42 +104,49 @@ public class CassSSTableReducer extends Reducer<CompositeKey, AtomWritable, Text
             // have all the columns a range tombstone applies to when we create
             // a snapshot. This will not be true if we go to partial incremental
             // processing
-            if (atom instanceof IColumn) {
-                IColumn column = (IColumn) atom;
-                // If the column is deleted by the rangeTombstone, just discard
-                // it, every other column of the same name will be discarded as
-                // well, unless it is later than the range tombstone in which
-                // case the column is out of date anyway
+            if (atom instanceof Column) {
+                Column column = (Column) atom;
                 if (this.tombstoneTracker.isDeleted(column)) {
-                    return;
-                }
-                if (currentColumn == null) {
+                    // If the column is deleted by the rangeTombstone, just discard
+                    // it, every other column of the same name will be discarded as
+                    // well, unless it is later than the range tombstone in which
+                    // case the column is out of date anyway
+                } else if (currentColumn == null) {
                     currentColumn = column;
-                    return;
                 } else if (currentColumn.name().equals(column.name())) {
-                    if (column.timestamp() > currentColumn.timestamp()) {
+                    if (column.timestamp() > currentColumn.minTimestamp()) {
                         currentColumn = column;
                     }
                 } else {
                     columns.add(currentColumn);
                     currentColumn = column;
                 }
+            } else if (atom instanceof RangeTombstone) {
+                // We do not include these columns in the output since they are deleted
+            } else {
+                throw new IllegalArgumentException("Cassandra added a new type " + atom.getClass().getCanonicalName() + " which we do not support");
             }
         }
-        
-        //TODO: expire columns
-        public void finalize() {
+
+        public void finalizeReduce() {
             if (currentColumn != null) {
                 columns.add(currentColumn);
             }
-        }
 
-        public void setDeletedAt(long deletedAt) {
-            this.deletedAt = deletedAt;
-        }
-
-        public void setKey(byte[] key) {
-            this.key = key;
+            // When cassandra compacts it removes columns that are in deleted rows
+            // that are older than the deleted timestamp.
+            // we will duplicate this behavior. If the etl needs this data at some
+            // point we can change, but it is only available assuming
+            // cassandra hasn't discarded it.
+            Iterator<OnDiskAtom> columnIterator = columns.iterator();
+            while (columnIterator.hasNext()) {
+                OnDiskAtom atom = columnIterator.next();
+                if (atom instanceof RangeTombstone) {
+                    columnIterator.remove();
+                } else if (atom instanceof Column && ((Column) atom).timestamp() <= this.deletedAt) {
+                    columnIterator.remove();
+                }
+            }
         }
 
         public void writeRow(DataOutput dos) throws IOException {
@@ -116,48 +165,13 @@ public class CassSSTableReducer extends Reducer<CompositeKey, AtomWritable, Text
             }
 
         }
-    }
 
-    private static final Logger LOG = LoggerFactory.getLogger(CassSSTableReducer.class);
-
-    FSDataOutputStream dos;
-
-    Reduce reduce;
-
-    @Override
-    protected void cleanup(Context context) throws IOException, InterruptedException {
-        dos.close();
-        super.cleanup(context);
-    }
-
-    @Override
-    public void reduce(CompositeKey key, Iterable<AtomWritable> values, Context ctx) throws IOException, InterruptedException {
-        reduce = new Reduce();
-        boolean first = true;
-        for (AtomWritable value : values) {
-            if (first) {
-                reduce.setKey(value.getKey());
-                reduce.setDeletedAt(value.getDeletedAt());
-            }
-            reduce.addAtom(value.getAtom());
+        public long getDeletedAt() {
+            return deletedAt;
         }
-        reduce.finalize();
-        reduce.writeRow(dos);
-        // TODO: batch this
-        ctx.getCounter("aegisthus", "rows_written").increment(1L);
-    }
 
-    @Override
-    protected void setup(Context context) throws IOException, InterruptedException {
-        StorageHelper sh = new StorageHelper(context);
-        Path outputDir = new Path(sh.getBaseTaskAttemptTempLocation());
-        FileSystem fs = outputDir.getFileSystem(context.getConfiguration());
-        String filename = String.format("%s-%s-%05d0%04d-Data.db",
-                context.getConfiguration().get("aegisthus.dataset", "keyspace-dataset"), Version.current_version,
-                context.getTaskAttemptID().getTaskID().getId(), context.getTaskAttemptID().getId());
-        Path outputFile = new Path(outputDir, filename);
-        LOG.info("writing to: {}", outputFile.toUri());
-        dos = fs.create(outputFile);
-        super.setup(context);
+        public void setDeletedAt(long deletedAt) {
+            this.deletedAt = deletedAt;
+        }
     }
 }
